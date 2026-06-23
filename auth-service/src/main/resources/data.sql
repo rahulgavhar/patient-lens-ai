dc4823996644d6bb72f1e3042aa0092fa18bd501
Rahul Gavhar <138155613+rahulgavhar@users.noreply.github.com>
2026-06-24 01:32:29 +0530

    feat: added integration tests before deployment

--- auth-service/src/main/resources/data.sql
+++ auth-service/src/main/resources/data.sql
@@ -33,3 +33,11 @@ SELECT '423e4567-e89b-12d3-a456-426614174008', 'john.doe', 'john@patient.com',
 WHERE NOT EXISTS (
     SELECT 1 FROM "users" WHERE email = 'john@patient.com' OR username = 'john.doe'
 );
+
+-- Seed TEST user
+INSERT INTO "users" (id, username, email, password, role)
+SELECT '523e4567-e89b-12d3-a456-426614174009', 'testuser', 'testuser@test.com',
+       '$2a$12$7hoRZfJrRKD2nIm2vHLs7OBETy.LWenXXMLKf99W8M4PUwO6KB7fu', 'ADMIN'
+WHERE NOT EXISTS (
+    SELECT 1 FROM "users" WHERE email = 'testuser@test.com' OR username = 'testuser'
+);
